e4c721ac314ca903026b24bffd02a4595a0c583a
nowzhao <nowszhao@foxmail.com>
2025-08-03 17:55:53 +0800

    update

--- src/utils/common.py
+++ src/utils/common.py
@@ -14,7 +14,6 @@ def generate_headers(request: dict, token: str) -> Dict[str, str]:
     cookie_string = "; ".join(cookie_parts)
     
     return {
-        #"Cookie": "hy_source=web;_qimei_h38=d941369c80e2f1043d10ddcf0300000e819613;hy_user=changhozhao;ptoken=EkzFI4pZ3iVJVzuwJ3SqcOphHY73Nqsm4IlYlNSAmIA;hy_token=8tE8bq6InCxff5mUqQZfc9aGHP6NPD80Cr/k258SiLJ9CYW8HiMzU5pREYyvnbvjeMlQugP5sjBBf6Z6HkeKPmw70gyim1uF7yzAGG5SktN5elniDcbIk281Qd3t9wBwmYiBi9omgQ/TZ8dmzLOH7OUJZkQAHF3eSZP9KHAu72idMXpzhtXSQZx/JRmqKbxiCqdogE6LIxYQxI7kTcyH2i/eZ2Jx15uRJSWJtjIdoDjkgDjvn9YbMM1X02xHbXdUF7uvIHykm5+8ViMhD0XcNOq3r2rl2ylh5kr0v3ggDZYj8dDgcN/VPRFbVlRnol2u4Rjl7PkwXh5c+SODtiVkLcBpNsp3PU7awhx6xouIVqsbsj8yEcLETtOLizXZArugjYbJ9G2UgnfPpbI04U1aHfpuEohb1Y1ZMhxMMpJ0L6LH/ms1OG8t236CqInb1ZbHLACoCZXCgpii7cCAjOkw4iGmYp3sCi9Yj5BDj7voQfk1AgUr+hNeMQwrSdDUUNNfS4Z4aPeUBRttX1NlRgQkwslwKE+wKsq1qMtYGzxXTBRngTXbrPRPBs/UApNSaqelCvscmMfLdZK9bOBo5+NXCQ==;_qimei_i_1=4fea5dd69c5e51d8c79ead385bd171b6f6eea0f2465a03d6e0dc7e582593206c6163629d3980e4ddd5bfd481;ttwid=1%7CuG4oFBnsgZm9i72IikAphhzYWGJe87c8gt0RaX3Yb9Y%7C1751263005%7C8a9c16fccc0c3072b312a8d4eaa5092c5d6b4e99d205f85298121621322cc9b0;",
         "Cookie": cookie_string,
         "Origin": "https://yuanbao.tencent.com",
         "Referer": f"https://yuanbao.tencent.com/chat/{request['agent_id']}",
--- test.py
+++ test.py
@@ -6,15 +6,14 @@ from openai import OpenAI
 base_url = "http://localhost:8000/v1/"
 
 hy_source = "web"
-hy_user = "changhozhao"    # 替换为你的用户ID
-hy_token = "8tE8bq6InCxff5mUqQZfc9aGHP6NPD80Cr/k258SiLJ9CYW8HiMzU5pREYyvnbvjeMlQugP5sjBBf6Z6HkeKPmw70gyim1uF7yzAGG5SktN5elniDcbIk281Qd3t9wBwmYiBi9omgQ/TZ8dmzLOH7OUJZkQAHF3eSZP9KHAu72idMXpzhtXSQZx/JRmqKbxiTliC8SW41io0pjrb9niVa/gJ4evBkQl469MZV55jssUPevs9JscuwK9hmWjEbaylii47iTa/VI5sJLzwUSlCNGyAJaruRn5chBmSt0V9OQBY7Thw75ot0KoVFf6aFaLheqW0RXRoU+jiA8qK8NSIX7r1LtedDIEwfJI6s4hP2o81Xny5qHoaPFEwV5h4SiT2sn2/5n3JbHWuDszUVlg3PTOoquchMar4jK0vspxWIsrqcSixQjSv9+t/nEkNzevd6h8/s2rf2t6HIg0prAU9MzWLVnl4wLgVn5KLEmYgzTUJasKG9Xa6fdrFDXlksdbJQOXMVVvOcWOqPN/e3OYjAM9GeVPM8bWJmgMXofZz3KKtDwGVMgvdSFZ3FUn986KhpSkY+mHmx6OgL82Hiu4tQA=="   # 替换为你的token
+hy_user = ""    # 替换为你的用户ID
+hy_token = ""   # 替换为你的token
 
 # 新增的Cookie字段（可选，如果有的话可以添加）
-_qimei_h38 = "d941369c80e2f1043d10ddcf0300000e819613"  # 可选
-ptoken = "EkzFI4pZ3iVJVzuwJ3SqcOphHY73Nqsm4IlYlNSAmIA"  # 可选
-_qimei_i_1 = "4fea5dd69c5e51d8c79ead385bd171b6f6eea0f2465a03d6e0dc7e582593206c6163629d3980e4ddd5bfd481"  # 可选
-ttwid = "1%7CuG4oFBnsgZm9i72IikAphhzYWGJe87c8gt0RaX3Yb9Y%7C1751263005%7C8a9c16fccc0c3072b312a8d4eaa5092c5d6b4e99d205f85298121621322cc9b0"  # 可选
-
+_qimei_h38 = ""  # 可选
+ptoken = ""  # 可选
+_qimei_i_1 = ""  # 可选
+ttwid = ""  # 可选
 agent_id = "naQivTmsDa"
 chat_id = ""    # 可选，如果不提供会自动创建
 
